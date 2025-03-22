a187d18540e546ad114d9fdc069d226de85134e4
Erik de Lara <erikrodrigo680@gmail.com>
2025-03-22 10:50:34 -0300

    primeiro_projeto_py

---
+++ aula1.py
@@ -0,0 +1,8 @@
+#imput que solicita a frase ou palavra ao usuario
+texto = input("digite uma palavra ou frase: ")
+
+#contador de caracteres
+quantidade = len(texto)
+
+#Mostra o resultado
+print (f"A frase tem {quantidade} caracteres. ")
